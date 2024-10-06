ec66f81a4d54ce1ee0e8dc2a49de6419d76663fb
hammadsadi <hammad.sadi@yahoo.com>
2024-10-07 01:44:33 +0600

    Added Navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,3 @@
-import { ModeToggle } from "@/components/nav/mode-toggle";
-
 export default function Home() {
-  return (
-    <div>
-      <ModeToggle />
-    </div>
-  );
+  return <div></div>;
 }
