c76f54f840e5cbbb445b4cb42619f0429c8f3d12
hammadsadi <hammad.sadi@yahoo.com>
2024-10-07 23:57:19 +0600

    Step One Component added With Personal Information Form

--- app/resume/create/page.jsx
+++ app/resume/create/page.jsx
@@ -1,11 +1,23 @@
 "use client";
+import StepFive from "@/components/resume/step-five";
+import StepFour from "@/components/resume/step-four";
+import StepOne from "@/components/resume/step-one";
+import StepThree from "@/components/resume/step-three";
+import StepTwo from "@/components/resume/step-two";
 import { useResume } from "@/context/resume";
 import React from "react";
 
 const CreateResumePage = () => {
-  const { resume } = useResume();
-  console.log(resume);
-  return <div></div>;
+  const { step, resume } = useResume();
+  return (
+    <div className="flex justify-center items-center h-screen">
+      {step === 1 && <StepOne />}
+      {step === 2 && <StepTwo />}
+      {step === 3 && <StepThree />}
+      {step === 4 && <StepFour />}
+      {step === 5 && <StepFive />}
+    </div>
+  );
 };
 
 export default CreateResumePage;
---
+++ components/resume/step-one.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useResume } from "@/context/resume";
+
+const StepOne = () => {
+  const { resume, setResume } = useResume();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(resume);
+  };
+  return (
+    <div className="w-full lg:w-1/2 p-5 border shadow rounded-lg">
+      <h2 className="text-2xl font-bold mb-2">Personal Information</h2>
+      <form onSubmit={handleSubmit}>
+        <Input
+          className="mb-3"
+          onChange={(e) => setResume({ ...resume, name: e.target.value })}
+          value={resume.name}
+          type="text"
+          autoFocus
+          placeholder="Full Name"
+          required
+        />
+        <Input
+          className="mb-3"
+          onChange={(e) => setResume({ ...resume, job: e.target.value })}
+          value={resume.job}
+          type="text"
+          placeholder="Job Title"
+          required
+        />
+        <Input
+          className="mb-3"
+          onChange={(e) => setResume({ ...resume, address: e.target.value })}
+          value={resume.address}
+          type="text"
+          placeholder="Your Address"
+          required
+        />
+        <Input
+          className="mb-3"
+          onChange={(e) => setResume({ ...resume, phone: e.target.value })}
+          value={resume.phone}
+          type="text"
+          placeholder="Your Number"
+          required
+        />
+        <Input
+          className="mb-3"
+          onChange={(e) => setResume({ ...resume, email: e.target.value })}
+          value={resume.email}
+          type="email"
+          placeholder="Your Email"
+          required
+        />
+        <div className="flex justify-end">
+          <Button>Save</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default StepOne;
