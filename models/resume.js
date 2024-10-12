a275e1d971b51d8ac41a011ebd4f1f9f84da4713
hammadsadi <hammad.sadi@yahoo.com>
2024-10-12 16:40:52 +0600

    Added Resume Schema

---
+++ models/resume.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+//  Experience
+const experienceSchema = new Schema({
+  title: String,
+  company: String,
+  address: String,
+  startDate: String,
+  endDate: String,
+  summery: String,
+});
+
+// Education
+const educationSchema = new Schema({
+  name: String,
+  address: String,
+  qualification: String,
+  year: String,
+});
+
+// Skills
+const skillsSchema = new Schema({
+  name: String,
+  label: String,
+});
+
+// Resume Data
+const resumeSchema = new Schema(
+  {
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    title: String,
+    name: String,
+    job: String,
+    address: String,
+    themeColors: String,
+    summery: String,
+    experience: [experienceSchema],
+    education: [educationSchema],
+    skills: [skillsSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default Resume =
+  mongoose.models.Resume || module("Resume", resumeSchema);
