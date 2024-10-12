ccbe4e362ea11b970a60969c2c687ddcdee1a23a
hammadsadi <hammad.sadi@yahoo.com>
2024-10-12 16:53:07 +0600

    Added Resume Create Server Action

---
+++ actions/resume/resume.js
@@ -0,0 +1,18 @@
+"use server";
+import connectWithDatabase from "@/config/db"; // Connect DB
+import Resume from "@/models/resume"; // Import Resume Model
+import { currentUser } from "@clerk/nextjs/dist/types/server"; // Get Loggedin User from clerk
+
+// Create Resume
+export const createResume = async (data) => {
+  try {
+    // Get User Info
+    const user = await currentUser();
+    const userEmail = user?.emailAddresses[0]?.emailAddress;
+    const { _id, ...rest } = data;
+    const resume = await Resume.create({ ...rest, userEmail });
+    return JSON.parse(JSON.stringify(resume));
+  } catch (error) {
+    throw new Error(error);
+  }
+};
