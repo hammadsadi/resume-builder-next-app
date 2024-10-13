ddb0b174ab26cd35ba1715ef963fddfbd9f7ea0d
hammadsadi <hammad.sadi@yahoo.com>
2024-10-14 01:36:27 +0600

    Added Skeleton

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,19 @@
 "use client";
+import SkeletonCard from "@/components/card/skeleton-card";
 import { useResume } from "@/context/resume";
 import React from "react";
 
 const DashboardPage = () => {
   const { userResume } = useResume();
+  if (!userResume?.length) {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <SkeletonCard />
+        <SkeletonCard />
+        <SkeletonCard />
+      </div>
+    );
+  }
   return (
     <div>
       <pre>{JSON.stringify(userResume, null, 4)}</pre>
